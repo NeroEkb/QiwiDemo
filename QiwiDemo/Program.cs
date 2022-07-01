9d4cfefe2eba5cd47edc91951502f2d3e913e7d2
NeroEkb <sema.egorow@gmail.com>
2022-07-01 22:19:56 +0500

    Добавьте файлы проекта.

---
+++ QiwiDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using QiwiDemo;
+using RestSharp;
+using Newtonsoft.Json;
+using System.Text.Json;
+using System.Collections.Generic;
+
+string token = ""; // Здесь ввоится токен
+List<Wallet> WalletList = new List<Wallet>(); // Создаём Лист аккаунтов
+WalletList.Add(new Wallet(token)); // Добавляем наш кошелёк
+foreach(Wallet wallet in WalletList) // Проходим по каждому кошельку в Листе
+{
+    Request requester = new Request(wallet); // Создаём запрос
+    var kek = JsonConvert.DeserializeObject<dynamic>( await requester.MakeRequestForNumber(wallet)); // Получаем в переменную результат ответа от сервера
+    wallet.Number = kek.contractInfo.contractId.ToString(); // переносим данные (номер телефона) из результата в экземпляр класса 
+    var k = JsonConvert.DeserializeObject<dynamic>(await requester.MakeRequestForBalance(wallet)); // Делаем запрос и получаем информацию о количестве и суммах кошельков
+
+    foreach (var account in k.accounts) // для каждого валютного кошелька переносим значения (валюта, сумма) в экземпляр класса 
+    {
+       wallet.Currency.Add(account.balance.currency, account.balance.amount);
+    }
+
+    Console.WriteLine(wallet.Currency);
+
+}
+
+Console.WriteLine(WalletList[0].Number);
---
+++ QiwiDemo/Request.cs
@@ -0,0 +1,64 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QiwiDemo
+{
+    internal class Request
+    {
+        private const string url = "https://edge.qiwi.com"; // ссылка на сервис
+        private readonly string accurl = "/person-profile/v1/profile/current?authInfoEnabled=True"; // ссылка для получения информации об аккаунте
+        private readonly string BalanceUrl = "/funding-sources/v2/persons/{number}/accounts"; // ссылка для получения информации о кошельках
+        private readonly RestClient client;
+
+        public Request(Wallet wallet)
+        {
+            client = new RestClient(); // при инициализации клиента создаём экземпляр
+        }
+
+        private RestRequest AddHeaders(RestRequest request, string token) // для работы запросов необходимы соответствующие заголовки 
+        {
+            request.AddHeader("Accept", "application/json");
+            request.AddHeader("Content-Type", "application/json");
+            request.AddHeader("Authorization", $"Bearer {token}");
+            return request;
+        }
+
+        public async Task<string> MakeRequestForNumber(Wallet wallet) // хоба, асинхронный метод в классе
+        {
+            RestRequest request = new RestRequest(url + accurl); // создаём запрос и корректируем ссылку
+            AddHeaders(request, wallet.Token); // добавляем заголовки
+            var response = await client.ExecuteAsync(request); // получаем ассинхронно ответ
+            string? result = null; // результат может быть null
+            if (response != null && response.StatusCode == System.Net.HttpStatusCode.OK) // Если ответ пиздат то
+            {
+                result = response.Content.ToString(); // в строку его
+            }
+
+            return result; // и на вывод
+        }
+
+        public async Task<string> MakeRequestForBalance(Wallet wallet) // то же самое что и выше, только для запроса баланса
+        {
+            string CurrentBalanceUrl = BalanceUrl.Replace("{number}", wallet.Number);
+            RestRequest request = new RestRequest(url + CurrentBalanceUrl);
+            AddHeaders(request, wallet.Token);
+            var respone = await client.ExecuteAsync(request);
+            string? result = null;
+            if (respone != null && respone.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                result = respone.Content.ToString();
+            }
+            return result;
+        }
+        
+        
+
+        
+
+    }
+}
