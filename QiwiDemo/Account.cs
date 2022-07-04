2b6da539b7769fd79113332793212cb9df09b9db
NeroEkb <sema.egorow@gmail.com>
2022-07-04 06:32:50 +0500

    minor changing

--- QiwiDemo/Account.cs
+++ QiwiDemo/Account.cs
@@ -10,11 +10,16 @@ namespace QiwiDemo
 {
     public class Account
     {
+
         public string Number { get; set; } // для хранения номера телефона
 
         public string Token { get; set; } // для токена
 
+        public string Nickname { get; set; } // название кошелька
+
         public string UserInfo { get; set; } // на всякий случай
+
+
         // хоба, коллекция <ключ, значение> для хранения неопределенного количества кошельков и их суммы
         public Dictionary<int, float> Wallet { get; set; } = new Dictionary<int, float>(); 
 
@@ -36,15 +41,14 @@ namespace QiwiDemo
         private async Task InitializeAsync(Account account)
         {
             Request requester = new Request(account); // Создаём запрос
-            var number = JsonConvert.DeserializeObject<dynamic>(await requester.MakeRequestForNumber(account)); // Получаем в переменную результат ответа от сервера
-            account.Number = number.contractInfo.contractId.ToString(); // переносим данные (номер телефона) из результата в экземпляр класса 
-            var k = JsonConvert.DeserializeObject<dynamic>(await requester.MakeRequestForBalance(account)); // Делаем запрос и получаем информацию о количестве и суммах кошельков
+            var response = JsonConvert.DeserializeObject<dynamic>(await requester.MakeRequestForNumber(account)); // Получаем в переменную результат ответа от сервера
+            account.Number = response.contractInfo.contractId.ToString(); // переносим данные (номер телефона) из результата в экземпляр класса 
+            account.Nickname = response.contractInfo.nickname.nickname.ToString();
+            response = JsonConvert.DeserializeObject<dynamic>(await requester.MakeRequestForBalance(account)); // Делаем запрос и получаем информацию о количестве и суммах кошельков
 
-            foreach (var acc in k.accounts) // для каждого валютного кошелька переносим значения (валюта, сумма) в экземпляр класса 
+            foreach (var acc in response.accounts) // для каждого валютного кошелька переносим значения (валюта, сумма) в экземпляр класса 
             {
-                int q = acc.balance.currency;
-                float z = acc.balance.amount;
-                account.Wallet.Add(q, z);
+                account.Wallet.Add((int)acc.balance.currency, (float)acc.balance.amount);
             }
         }
     }
--- QiwiDemo/Program.cs
+++ QiwiDemo/Program.cs
@@ -4,17 +4,31 @@ using Newtonsoft.Json;
 using System.Text.Json;
 using System.Collections.Generic;
 
-string token = ""; // Здесь ввоится токен
+
 List<Account> AccountList = new List<Account>(); // Создаём Лист аккаунтов
-AccountList.Add(await Account.CreateAccount(token)); // Добавляем наш кошелёк
+Console.WriteLine("Input token and press 'Enter', when done press 'Enter' again");
+string token;
+while(true)
+{
+    token = Console.ReadLine();
+    if (token.Length == 0) { Console.WriteLine("Empty string"); break; }
+    else
+    {
+        AccountList.Add(await Account.CreateAccount(token));
+    }
+    
+
+}
+
+
 foreach(Account account in AccountList) // Проходим по каждому кошельку в Листе
 {
-    Console.WriteLine(AccountList[0].Number);
-    foreach(KeyValuePair<int, float> wallet in AccountList[0].Wallet)
+    Console.WriteLine("Nickname = {0}, Number = {1}", account.Number, account.Nickname);
+    foreach(KeyValuePair<int, float> wallet in account.Wallet)
     {
         Console.WriteLine("currency = {0}, amount = {1}", wallet.Key, wallet.Value);
     }
 
 }
 
-
+Console.ReadKey();
